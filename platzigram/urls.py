07b7b7e5408f482d73d767cf2cf49a340c50747f
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-10-03 22:26:19 -0500

    query params

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,8 +1,11 @@
+"""Platzigram URLs module."""
+
 from django.urls import path
 from platzigram import views
 
 urlpatterns = [
 
     path('hello-world', views.hello_world),
+    path('hi', views.hi),
 
 ]
--- platzigram/views.py
+++ platzigram/views.py
@@ -1,4 +1,17 @@
+"""Platzigram views."""
+
+# Django
 from django.http import HttpResponse
 
+# Utilities
+from datetime import datetime
+
 def hello_world(request):
-    return HttpResponse('Hello, world!')
+    """Return a greeting."""
+    return HttpResponse('Oh, hi! Current server time is {now}'.format(
+        now=datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+    ))
+
+def hi(request):
+    """Hi."""
+    return HttpResponse(request.GET["numbers"])
