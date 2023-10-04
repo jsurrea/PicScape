0112434279713aebbfc2dfd1f05254ea59966664
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-10-04 18:07:43 -0500

    signup

--- users/views.py
+++ users/views.py
@@ -21,6 +21,31 @@ def login_view(request):
             return render(request, 'users/login.html', {'error': 'Invalid username and password'})
     return render(request, 'users/login.html')
 
+def signup(request):
+    """Sign up view."""
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in use'})
+            
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+        return redirect('login')
+    return render(request, 'users/signup.html')
+
 @login_required
 def logout_view(request):
     """Logout a user."""
