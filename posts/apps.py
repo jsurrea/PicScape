38eca6ed1dfd9d8a8dd587c4bf74fd0efe09d160
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-10-03 22:56:35 -0500

    apps

---
+++ posts/apps.py
@@ -0,0 +1,10 @@
+"""Posts application module."""
+
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    """Posts application settings."""
+    default_auto_field = "django.db.models.BigAutoField"
+    name = 'posts'
+    verbose_name = 'Posts'
