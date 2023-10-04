e783fa70c6eee4e714fb24a19f781ca3b0199b89
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-10-04 01:21:27 -0500

    admin

---
+++ users/admin.py
@@ -0,0 +1,28 @@
+"""User admin classes."""
+
+# Django
+from django.contrib import admin
+
+# Models
+from users.models import Profile
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Profile admin."""
+
+    list_display = ("pk", "user", "phone_number", "website", "picture")
+    list_display_links = ("pk", "user")
+    list_editable = ("phone_number", "website", "picture")
+    search_fields = (
+        "user__email", 
+        "user__username",
+        "user__first_name", 
+        "user__last_name", 
+        "phone_number"
+    )
+    list_filter = (
+        "created", 
+        "modified", 
+        "user__is_active", 
+        "user__is_staff"
+    )
---
+++ users/apps.py
@@ -0,0 +1,10 @@
+"""Users app configuration."""
+
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    """User app config."""
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "users"
+    verbose_name = "Users"
