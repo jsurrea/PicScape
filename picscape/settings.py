e84378e0243aad3c5d2925c48274386b099acd1e
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-10-06 11:49:51 -0500

    fix redirect on deployment

--- picscape/settings.py
+++ picscape/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-LOGIN_URL = "login/"
+LOGIN_URL = "users:login"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
