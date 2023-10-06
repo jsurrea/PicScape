ffd72f3f17faffd1fa088c5b78aec0ea6c807cdf
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-10-06 12:12:48 -0500

    update settings

--- picscape/settings.py
+++ picscape/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('PICSCAPE_SECRET_KEY', 'django-insecure-pcd#=y&javeo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
